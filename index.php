e47d63e1c4212138037fbbd1ae9e42358c919487
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-06 17:30:05 +0200

    chore: Update CSS file extension to .scss and improve ToDoList layout

--- index.php
+++ index.php
@@ -7,22 +7,25 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link grity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="scss/style.css">
+    <link rel="stylesheet" href="scss/style.scss">
     <script src="https://unpkg.com/axios@1.6.7/dist/axios.min.js"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="js/script.js" defer></script>
-    <title>Our Team</title>
+    <title>ToDoList</title>
 
 </head>
 
 <body>
     <div id="app">
-        <input v-model="newTodo" @keyup.enter="addTodo">
-        <ul>
-            <li v-for="(todo, index) in todos">
-                <span :class="{ completed: todo.completed }" @click="toggleCompleted(index)">{{ todo.text }}</span>
-                <button @click="removeTodo(index)">Rimuovi</button>
-            </li>
-        </ul>
+        <div class="container">
+            <h1>ToDoList</h1>
+            <input v-model="newTodo" @keyup.enter="addTodo" class="todo-input">
+            <ul>
+                <li v-for="(todo, index) in todos" class="todo-item">
+                    <span :class="{ completed: todo.completed }" @click="toggleCompleted(index)">{{ todo.text }}</span>
+                    <button @click="removeTodo(index)">Rimuovi</button>
+                </li>
+            </ul>
+        </div>
     </div>
 </body>
